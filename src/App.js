83bd293c407943f7ff326477ddaa2069d68d4bab
Matti Cosentino <mattocosen@hotmail.com>
2022-06-08 21:23:15 -0300

    add states to proyect

--- src/App.js
+++ src/App.js
@@ -5,22 +5,43 @@ import { TodoSearch } from "./TodoSearch";
 import { CreateTodoButton } from "./CreateTodoButton";
 import { TodoList } from "./TodoList";
 
-const todos = [
-  { text: "cortar cebolla", completed: false },
+const defaultTodos = [
+  { text: "cortar cebolla", completed: true },
   { text: "tomar curso de React", completed: false },
   { text: "Llorar con la llorona", completed: false },
+  { text: "Crear un nuevo estado", completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+  const completedTodos = todos.filter((todo) => !!todo.completed.length);
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
 
-      <TodoSearch />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {todos.map((todo) => (
-          <TodoItem text={todo.text} />
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./TodoCounter.css";
 
-function TodoCounter() {
-  return <h2 className="TodoCounter">Has completado un pendiente</h2>;
+function TodoCounter({ total, completed }) {
+  return (
+    <h2 className="TodoCounter">
+      Has completado {completed} de un {total} pendientes
+    </h2>
+  );
 }
 
 export { TodoCounter };
